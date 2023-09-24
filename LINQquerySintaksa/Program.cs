16ba139a4e4dc3f1fcc544b1e87b68b21cc7f9c3
Pat <113007904+PattZac@users.noreply.github.com>
2023-09-24 18:55:46 +0200

    Add files via upload

---
+++ LINQquerySintaksa/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Student collection
+        List<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John" } ,
+                new Student() { StudentID = 2, StudentName = "Moin" } ,
+                new Student() { StudentID = 3, StudentName = "Bill" } ,
+                new Student() { StudentID = 4, StudentName = "Ram" } ,
+                new Student() { StudentID = 5, StudentName = "Ron"  }
+            };
+
+        // upit i izvršavanje upita
+
+        var query = from s in studentList
+                    where s.StudentName.Contains('o')
+                    select s;
+
+        foreach( var s in query )
+        {
+            Console.WriteLine( s.StudentName );
+        }
+
+        Console.ReadLine();
+
+    }
+}
+
+public class Student
+{
+
+    public int StudentID { get; set; }
+    public string StudentName { get; set; }
+}
